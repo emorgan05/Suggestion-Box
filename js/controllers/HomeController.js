0e883f5bbf82aa2a8d12ccaa5b3fbddfaa87958a
emorgan05 <emorgan05@gmail.com>
2016-10-16 16:57:45 -0400

    Add files via upload

---
+++ js/controllers/HomeController.js
@@ -0,0 +1,20 @@
+app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
+	$scope.helloWorld = "Hello, AngularJS!";
+	$scope.posts = suggestions.posts;
+	$scope.addSuggestion = function() {
+		if(!$scope.title || $scope.title === "") {
+			return;
+		}
+
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes: 0,
+		});
+
+		$scope.title = '';
+
+	};
+	$scope.upVote = function(index) {
+		$scope.posts[index].upvotes +=1;
+	};
+}]);
