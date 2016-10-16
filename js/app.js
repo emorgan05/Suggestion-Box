490a7db766deaa2a27c9fd1b7a783140165dd2c1
emorgan05 <emorgan05@gmail.com>
2016-10-16 17:08:07 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
 var app = angular.module('SuggestionBox', []);
-
-app.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			controller: 'HomeController',
-			templateUrl: 'views/home.html'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-});
